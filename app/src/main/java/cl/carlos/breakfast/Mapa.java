9e7e86c4ea42125bdec79fd0ecf67fb4ded1cbed
CarlosCaro2020 <75224376+CarlosCaro2020@users.noreply.github.com>
2020-11-30 04:21:38 -0300

    Initial commit

---
+++ app/src/main/java/cl/carlos/breakfast/Mapa.java
@@ -0,0 +1,14 @@
+package cl.carlos.breakfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mapa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+    }
+}
